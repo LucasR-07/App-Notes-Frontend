72ba8243b18b5345a6c7e479a590b8bee0e5b611
LucasR-07 <lucasromero2705@gmail.com>
2020-10-01 17:55:02 -0300

    add Loading Compontent

--- src/components/NotesList.jsx
+++ src/components/NotesList.jsx
@@ -2,10 +2,12 @@ import React, { Component } from "react";
 import axios from "axios";
 import { format } from "timeago.js";
 import { Link } from "react-router-dom";
+import Loading from "./Loading";
 
 export default class NotesList extends Component {
   state = {
     notes: [],
+    loading: false,
   };
 
   componentDidMount() {
@@ -13,46 +15,64 @@ export default class NotesList extends Component {
   }
 
   async getNotes() {
-    const res = await axios.get("https://appnotes-backend.herokuapp.com/api/notes");
-    this.setState({ notes: res.data });
+    this.setState({ loading: true })
+    const res = await axios.get(
+      "https://appnotes-backend.herokuapp.com/api/notes"
+    );
+    this.setState({ notes: res.data, loading: false });
   }
 
   deleteNote = async (id) => {
-    await axios.delete("https://appnotes-backend.herokuapp.com/api/notes/" + id);
+    await axios.delete(
+      "https://appnotes-backend.herokuapp.com/api/notes/" + id
+    );
     this.getNotes();
   };
 
   render() {
-    return (
-      <div className="row">
-        {this.state.notes.map((note) => (
-          <div key={note._id} className="col-md-4 p-2">
-            <div className="card bg-dark text-light shadow-lg border-0">
-              <div className="card-header d-flex justify-content-between">
-                <h5><b>{note.title}</b></h5>
-                <Link className="btn btn-danger text-light rounded-pill" to={"/notes/" + note._id}>
-                  Edit
-                </Link>
-              </div>
+    if (this.state.loading) {
+      return (
+        <Loading />
+      )
+    } else {
+      return (
+        <div className="row">
+          {this.state.notes.map((note) => (
+            <div key={note._id} className="col-md-4 p-2">
+              <div className="card bg-dark text-light shadow-lg border-0">
+                <div className="card-header d-flex justify-content-between">
+                  <h5>
+                    <b>{note.title}</b>
+                  </h5>
+                  <Link
+                    className="btn btn-danger text-light rounded-pill"
+                    to={"/notes/" + note._id}
+                  >
+                    Edit
+                  </Link>
+                </div>
 
-              <div className="card-body">
-                <p>{note.content}</p>
-                <p>User: <b>{note.author}</b></p>
-                <p>{format(note.date)}</p>
-              </div>
+                <div className="card-body">
+                  <p>{note.content}</p>
+                  <p>
+                    User: <b>{note.author}</b>
+                  </p>
+                  <p>{format(note.date)}</p>
+                </div>
 
-              <div className="card-footer">
-                <button
-                  className="btn btn-danger text-light"
-                  onClick={() => this.deleteNote(note._id)}
-                >
-                  Delete
-                </button>
+                <div className="card-footer">
+                  <button
+                    className="btn btn-danger text-light"
+                    onClick={() => this.deleteNote(note._id)}
+                  >
+                    Delete
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
-    );
+          ))}
+        </div>
+      );
+    }
   }
 }
